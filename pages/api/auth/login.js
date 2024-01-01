921c1bfe14b35146e6a3070b6ed9e0c4111d797c
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-01-01 23:41:31 +0300

    add login next-connect

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,12 +1,19 @@
+
+
+import { prisma } from "../../../src/lib/prisma";
 import jwt from "jsonwebtoken";
 import { setCookie } from "cookies-next";
 import bcrypt from "bcrypt";
-import { prisma } from "../../../src/lib/prisma";
+import nc from 'next-connect'
+const handler = nc()
 
-export default async (req, res) => {
-  const { email, password } = req.body;
+const EMAIL_REGEX = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
-  if (req.method === "POST") {
+
+handler.post(async (req, res) => {
+
+    const { email, password } = req.body;
+
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (!user) return res.status(422).json({ message: "Wrong email!" });
@@ -15,10 +22,7 @@ export default async (req, res) => {
       return res.status(422).json({ message: "Wrong password!" });
 
 
-       // Set CORS headers
-       res.setHeader('Access-Control-Allow-Origin', '*');
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-       res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+
 
 
 
@@ -35,7 +39,75 @@ export default async (req, res) => {
     });
 
     res.status(200).json(user);
-  } else {
-    res.status(424).json({ message: "Invalid method!" });
-  }
-};
+  } 
+  
+ 
+
+
+
+ 
+)
+
+
+
+
+
+
+
+
+
+export default handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import jwt from "jsonwebtoken";
+// import { setCookie } from "cookies-next";
+// import bcrypt from "bcrypt";
+// import { prisma } from "../../../src/lib/prisma";
+
+// export default async (req, res) => {
+//   const { email, password } = req.body;
+
+//   if (req.method === "POST") {
+//     const user = await prisma.user.findUnique({ where: { email } });
+
+//     if (!user) return res.status(422).json({ message: "Wrong email!" });
+//     const rightPassword = await bcrypt.compare(password, user.password);
+//     if (!rightPassword)
+//       return res.status(422).json({ message: "Wrong password!" });
+
+
+
+
+
+
+
+//     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+//       expiresIn: "7d",
+//     });
+
+//     setCookie("token", token, {
+//       req,
+//       res,
+//       maxAge: 60 * 60 * 24 * 7, // 7 days
+//       path: "/",
+//     });
+
+//     res.status(200).json(user);
+//   } else {
+//     res.status(424).json({ message: "Invalid method!" });
+//   }
+// };
