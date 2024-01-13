c547165efb71203907a3e163620c02f165f07def
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-01-13 17:18:28 +0300

    addd

--- src/lib/globall.js
+++ src/lib/globall.js
@@ -2,7 +2,7 @@ export const ImageEndpoint ="https://dash93.nyc3.cdn.digitaloceanspaces.com"
 export const defaultImage = "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
 
 
-const REACT_APP_BASE_URL1 = "https://file-uploader-red.vercel.ap";
+const REACT_APP_BASE_URL1 = "https://file-uploader-red.vercel.app";
 const REACT_APP_BASE_URL = "http://localhost:8000";
 
 export const uploadApi =
