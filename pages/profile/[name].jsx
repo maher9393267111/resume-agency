e2568240cc47e863f306a1a0a11c8d4852341f0a
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-02-14 13:36:38 +0300

    add

--- pages/profile/[name].jsx
+++ pages/profile/[name].jsx
@@ -354,7 +354,7 @@ const CreateVCard= () => {
   vcard.socialUrls['facebook'] = about.facebook ? about.facebook : null;
     vcard.socialUrls['linkedIn'] = about?.linkedin ? about?.linkedin : null;
     vcard.socialUrls['twitter'] = about?.twitter ? about?.twitter : null;
-
+    vcard.socialUrls['instagram'] = about?.instagram ? about?.instagram : null;
   // console.log(vcard.getFormattedString());
   // vcard.photo.attachFromUrl( about?.myImage ? about?.myImage : 'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745', 'JPEG');
 
